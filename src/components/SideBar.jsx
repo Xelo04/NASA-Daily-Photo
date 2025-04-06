801b013b0158f4f6a90d653a1b20a675d894f4db
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-04-06 17:34:33 +0200

    Enhance Footer and SideBar components with background overlays; update CSS for improved layout and styling

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,14 +1,17 @@
 export default function SideBar() {
   return (
-    <>
-      <h2>The Brutal Martian Landscape</h2>
-      <div>
-        <p>Description</p>
-        <p>
-          Description dadada
-          asssssssssssssssssssssssssssssssssssssssssssssssssssssss
-        </p>
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContent">
+        <h2>The Brutal Martian Landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>
+            Description dadada
+            asssssssssssssssssssssssssssssssssssssssssssssssssssssss
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
